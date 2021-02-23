c1e852386c15109760b4f237ab6291eee92cf4c5
ac-04 <carloshernandez.isaias12.2@gmail.com.com>
2021-02-23 16:06:40 -0600

    :sparkles: adding vue to the project

---
+++ SoccerSmash/srcvue/webapp/app/Home/Home.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import HomeComponent from "./Components/HomeComponent.vue";
+
+const app = new Vue({
+    "el":"#homeComponent",
+    components:{HomeComponent}
+})
---
+++ SoccerSmash/webpack.mix.js
@@ -0,0 +1,29 @@
+let mix = require('laravel-mix');
+
+mix.autoload({
+    axios:['$axios']
+})
+
+mix.js('srcvue/webapp/app/Home/Home.js', 'wwwroot/js').vue()
+    .webpackConfig({
+        module: {
+            rules: [
+                // We're registering the TypeScript loader here. It should only
+                // apply when we're dealing with a `.ts` or `.tsx` file.
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    options: { appendTsSuffixTo: [/\.vue$/] },
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+
+        resolve: {
+            // We need to register the `.ts` extension so Webpack can resolve
+            // TypeScript modules without explicitly providing an extension.
+            // The other extensions in this list are identical to the Mix
+            // defaults.
+            extensions: ['*', '.js', '.jsx', '.vue', '.ts', '.tsx'],
+        },
+    });
